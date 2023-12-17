2ab69078896c5d941ebd10ad514bd8e670938d6f
Joseph Salvatori <joe@zero.nyc>
2023-12-17 12:46:17 -0600

    remove db

--- src/lib/integrations/index.js
+++ src/lib/integrations/index.js
@@ -2,18 +2,15 @@
  * Initial API configurations
  */
 import Content from "$lib/integrations/prismic";
-import Database from "$lib/integrations/mongodb";
 
 const config = {
 	content: new Content(),
-	database: new Database()
 }
 
 class ApiClass {
 
 	constructor() {
 		this.Content = config.content;
-		this.Database = config.database;
 	}
 }
 
