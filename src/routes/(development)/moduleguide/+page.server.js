fc72cf9089b2cea9b041d6d5ba9dc71f856434c1
Joseph Salvatori <joseph.salvatori@gmail.com>
2024-02-27 19:59:38 -0600

    guide

--- src/routes/(development)/moduleguide/+page.server.js
+++ src/routes/(development)/moduleguide/+page.server.js
@@ -1,6 +1,6 @@
 import { error, redirect } from "@sveltejs/kit";
 
-export const prerender = true;
+export const prerender = false;
 
 /** @type {import('./$types').LayoutServerLoad} */
 export async function load({ cookies, fetch, params, request, url }) {
