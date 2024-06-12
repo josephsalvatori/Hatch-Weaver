1fd585aba25fa90b3200f1ab8e5dfdb5f3895777
Joseph Salvatori <joseph.salvatori@gmail.com>
2024-06-12 10:51:21 -0500

    update for splash

--- src/routes/(pages)/+page.svelte
+++ src/routes/(pages)/+page.svelte
@@ -5,8 +5,17 @@ import SliceSwitch from "$lib/components/utilities/SliceSwitch.svelte";
 export let data;
 </script>
 
-{#key data.page.id}
+<div class="min-h-screen grid items-center w-full bg-blue-50">
+	<div class="flex items-center justify-center mx-auto">
+		<div class="richtext max-w-[800px]">
+			<h1>Currently incubating.</h1>
+			<p>Getting ready to hatch soon.</p>
+		</div>`
+	</div>
+</div>
+
+<!-- {#key data.page.id}
 	{#each data.page.data.slices as section, i}
 		<SliceSwitch slice={section} index={i} />
 	{/each}
-{/key}
+{/key} -->
